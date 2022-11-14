4053e236d168761ab546f63119556abc29f09009
Michał Murawski <m.a.murawski@wp.pl>
2022-11-14 21:39:45 +0100

    http request

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries.js';
+
+function fetchTest(name) {
+  fetchCountries(name)
+    .then(country => {
+      console.log(country.length);
+      console.log(country[0].name);
+      console.log(country[0].capital[0]);
+      console.log(country[0].population);
+      console.log(country[0].flags.svg);
+      console.log(country[0].languages);
+    })
+    .catch(error => console.log(error));
+}
+
+const test = fetchTest('pola');
 
 const DEBOUNCE_DELAY = 300;
