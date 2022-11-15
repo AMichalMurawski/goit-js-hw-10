5cb4227f6131773dee87e871af482b96e3591b1a
Michał Murawski <m.a.murawski@wp.pl>
2022-11-15 01:21:13 +0100

    end task

--- src/index.js
+++ src/index.js
@@ -1,19 +1,87 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import { fetchCountries } from './fetchCountries.js';
 
-function fetchTest(name) {
+const DEBOUNCE_DELAY = 300;
+const countryInput = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
+
+countryInput.addEventListener(
+  'input',
+  debounce(event => {
+    const name = trimName(event.target.value);
+    name !== '' ? fetchCountry(name) : null;
+  }, DEBOUNCE_DELAY)
+);
+
+function fetchCountry(name) {
   fetchCountries(name)
     .then(country => {
-      console.log(country.length);
-      console.log(country[0].name);
-      console.log(country[0].capital[0]);
-      console.log(country[0].population);
-      console.log(country[0].flags.svg);
-      console.log(country[0].languages);
+      informationAboutCountry(country);
     })
-    .catch(error => console.log(error));
+    .catch(() => {
+      countryList.innerHTML = '';
+      countryInfo.innerHTML = '';
+      Notiflix.Notify.failure('Oops, there is no country with that name');
+    });
 }
 
-const test = fetchTest('pola');
+function informationAboutCountry(country) {
+  if (country.length > 10) {
+    return toManyCountries();
+  }
+  if (country.length > 1) {
+    return addCountriesToList(country);
+  }
+  return showCountry(country);
+}
 
-const DEBOUNCE_DELAY = 300;
+function toManyCountries() {
+  countryList.innerHTML = '';
+  countryInfo.innerHTML = '';
+  Notiflix.Notify.info(
+    'Too many matches found. Please enter a more specific name.'
+  );
+}
+
+function addCountriesToList(countries) {
+  countryInfo.innerHTML = '';
+  const markup = countries
+    .sort((firstCountry, secondCountry) =>
+      firstCountry.name.common.localeCompare(secondCountry.name.common)
+    )
+    .map(
+      country =>
+        `<li class="country-item">
+           <img src="${country.flags.svg}" />
+           <p>${country.name.common}</p>
+         </li>`
+    )
+    .join('\n');
+  countryList.innerHTML = markup;
+}
+
+function showCountry(country) {
+  countryList.innerHTML = '';
+  const languages = Object.values(country[0].languages).join(', ');
+  const markup = country.map(
+    country =>
+      `<h3><img src="${country.flags.svg}" />${country.name.common}</h3>
+         <p><b>Capital: </b>${country.capital}</p>
+         <p><b>Population: </b>${country.population}</p>
+         <p><b>Laungages: </b>${languages}</p>`
+  );
+  countryInfo.innerHTML = markup;
+}
+
+function trimName(name) {
+  let newName = '';
+  name.split(' ').forEach((element, index) => {
+    if (element !== '') {
+      newName += ' ' + element;
+    }
+  });
+  return newName.trim();
+}
